dbe35c3e665469e9df9334139c244fe2c3d9efc2
Osvaldo Valentin <69062379+ValentinOsvaldo@users.noreply.github.com>
2022-06-30 17:02:58 -0500

    fix

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,10 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to 
-# git push -f git@github.com:valentinosvaldo/todo-app.git main:gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:valentinosvaldo/todo-app.git main:gh-pages
 
 cd -
